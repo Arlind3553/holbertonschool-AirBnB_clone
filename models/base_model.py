193276c03b7939b5affb141c66faf44bf2151874
Arlind3553 <georgetesco@yahoo.com>
2023-10-31 04:58:59 -0700

    task 1

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,16 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        self.created_at = self.updated_at = datetime.datetime.now()
 
     def save(self):
         self.updated_at = datetime.datetime.now()
