d7c747fabd39e36249a6d7f6658730473a366bf5
Holberton1990 <ersida9019@gmail.com>
2023-11-02 12:10:24 +0000

    console

--- console.py
+++ console.py
@@ -1,1 +1,36 @@
+#!/usr/bin/python3
+"""
+Entry point of the command interpreter
+"""
 
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBC
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Empty line + enter
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
