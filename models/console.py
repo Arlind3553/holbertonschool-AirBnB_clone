04e81209910bfcbf66d0ae4e634c247e3bc1e7ac
Holberton1990 <ersida9019@gmail.com>
2023-11-02 11:51:23 +0000

    console

--- models/console.py
+++ models/console.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Entry point of the command interpreter
+"""
+
+
 import cmd
 
 
@@ -6,12 +11,21 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def quit(self, arg):
+         """
+        Quit command to exit the program
+        """
         return True
 
     def EOF(self, arg):
+        """
+        EOF command to exit the program
+        """
         return True
 
     def emptyline(self):
+         """
+        Empty line + enter
+        """
         pass
 
 if __name__ == '__main__':
