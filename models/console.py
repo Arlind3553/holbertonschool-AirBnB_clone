0f22605e71c1695554608d1e7a7404279a61d127
Holberton1990 <ersida9019@gmail.com>
2023-11-02 11:20:58 +0000

    console

---
+++ models/console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def quit(self, arg):
+        return True
+
+    def EOF(self, arg):
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
